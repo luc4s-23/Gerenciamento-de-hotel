6fb6cfa8577d8b91507c0844646418eff6ab0fd3
lucas daniel <devlucasdaniel@gmail.com>
2025-11-02 20:18:24 -0300

    criando metodo http para post

--- API/Controller/HospedeController.cs
+++ API/Controller/HospedeController.cs
@@ -1,3 +1,4 @@
+using Hoteis.API.Model;
 using Hoteis.API.Service;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,6 +12,35 @@ namespace Hoteis.API.Controller
         public HospedeController(IHospedeService hospedeService)
         {
             _hospedeService = hospedeService;
+        }
+
+        [HttpPost("novo-hospede")]
+        public async Task<IActionResult> NovoRegistro(HospedeDTO dto)
+        {
+            var resultado = await _hospedeService.ValidarHospedeAsync(hospede);
+
+            return resultado.Status switch
+            {
+                200 => Ok(resultado.MensagemOuObjeto),
+                400 => BadRequest(resultado.MensagemOuObjeto),
+                409 => Conflict(resultado.MensagemOuObjeto),
+                _ => StatusCode(500, "Erro inesperado.")
+            };
+
+            if (dto == null)
+            {
+                return BadRequest();
+            }
+            var NovoRegistro = new Hospede
+            {
+                Nome_hospede = dto.Nome_hospede,
+                Email_hospede = dto.Email_hospede,
+                CPF_hospede = dto.CPF_hospede,
+                Telefone_hospede = dto.Telefone_hospede,
+                Menor_idade = dto.Menor_idade
+            };
+            await _hospedeService.AdicionarHospedeAsync(NovoRegistro);
+            return Ok(NovoRegistro);
         }
 
         [HttpGet("listar-todos-registros")]

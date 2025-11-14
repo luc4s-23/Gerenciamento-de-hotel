e49f05e593c286316ee6330063f1d60ae4d2ac2e
lucas <devlucasdaniel@gmail.com>
2025-11-14 14:12:33 -0300

    alterando relação de entidades 'Hospede para Reserva & Quarto para Reserva'

--- API/Model/Quarto.cs
+++ API/Model/Quarto.cs
@@ -7,10 +7,15 @@ namespace Hoteis.API.Model
     {
         [Key]
         public int Id_quarto { get; set; }
+        [Required]
         public int Numero_quarto { get; set; }
+        public ICollection<Reserva> reservas { get; set; }
+        [Required]
         public string Tipo { get; set; } = string.Empty;
+        [Required]
         public int Capacidade { get; set; }
         [Precision(10, 2)]
+        [Required]
         public decimal Preco_quarto_diaria { get; set; }
         public string Status { get; set; } = string.Empty;
         public string Descrição { get; set; } = string.Empty; //Deverá ser usado para informar mais detalhes sobre o quarto e o que ele oferece
--- API/Model/Reserva.cs
+++ API/Model/Reserva.cs
@@ -8,12 +8,12 @@ namespace Hoteis.API.Model
     {
         [Key]
         public int Id_reserva { get; set; }
-        public string CPF_Hospede { get; set; } = string.Empty;
         public DateTime Data_entrada { get; set; }
         public DateTime Data_saida { get; set; }
         public int Quantidade_hospedes { get; set; }
-        public int MyProperty { get; set; }
+        public Quarto Quarto { get; set; }
         public int Quarto_ID_FK { get; set; }
+        public Hospede hospede { get; set; }
         public int Hospede_ID_FK { get; set; }
         [Precision(10, 2)]
         public decimal Preco_total { get; set; }
